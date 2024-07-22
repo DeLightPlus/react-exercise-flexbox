49bb7b651201a79e4d434363b5af981a4f4ec1ca
DeLightPlus <110693341+DeLightPlus@users.noreply.github.com>
2024-07-22 08:02:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App()
+{
+  return (
+    <div className="App">
+      <div className="FlexBox">
+
+        <div className='row one'>
+          <div className='item'>
+
+          </div>
+
+          <div className='item mid'>
+
+          </div>
+
+          <div className='item '>
+
+          </div>
+
+        </div>
+
+        <div className='row-central'>
+              <div className='item'>
+
+              </div>
+
+              <div className='item mid'>
+                <div className='circle'>
+                  
+                </div>
+              </div>
+
+              <div className='item '>
+
+              </div>
+
+        </div>
+
+        <div className='row three'>
+          <div className='item '>
+
+          </div>
+
+          <div className='item mid'>
+
+          </div>
+
+          <div className='item '>
+
+          </div>
+
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
